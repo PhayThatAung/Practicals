ce815701d1bb5032bfd6959b17270e72cff09d4c
Ricky <phaythataung@my.jcu.edu.au>
2022-11-27 21:55:27 +0800

    Add password check program

---
+++ Prac_02/score_menu.py
@@ -0,0 +1,45 @@
+menu = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+score = -1
+
+print(menu)
+choice = input("Choice: ").lower()
+while choice != "q":
+    if choice == "g":
+        score = float(input("Score: "))
+        print(menu)
+        choice = input("Choice: ").lower()
+        while score < 0 or score > 100:
+            print(f"Invalid score")
+            score = float(input("Score: "))
+
+    elif choice == "p":
+        if score >= 90:
+            print(f"Excellent")
+        elif score >= 50:
+            print(f"Passable")
+        elif score >= 0:
+            print(f"Bad")
+        else:
+            print(f"You have not inputted any number, please input a number at the menu")
+        print(menu)
+        choice = input("Choice: ").lower()
+
+    elif choice == "s":
+        if score == -1:
+            print(f"You have not inputted any number, please input a number at the menu")
+            print(menu)
+        else:
+            print(f"{'*' * int(score / 10)}")
+            print(menu)
+        choice = input("Choice: ").lower()
+
+    else:
+        print(f"Invalid Choice")
+        print(menu)
+        choice = input("Choice: ").lower()
+
+
+print(f"Finished")
---
+++ Prac_02/temperatures.py
@@ -0,0 +1,37 @@
+menu = """C - Celsius to Fahrenheit
+F - Fahrenheit to Celsius
+Q - Quit
+"""
+
+
+def main():
+    print(menu)
+    choice = input(f"Choice: ").lower()
+
+    while choice != "q":
+        if choice == "c":
+            celsius = float(input(f"Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} fahrenheit")
+        elif choice == "f":
+            fahrenheit = float(input(f"Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} celsius")
+        else:
+            print(f"Invalid Choice")
+        print(menu)
+        choice = input(f"Choice: ").lower()
+    print(f"Program Ended")
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+main()
